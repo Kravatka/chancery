1eae0ad118815d66a72dffbc7b3f405264b581cf
egor <Egor.Kravchenko@tpg.ua>
2018-01-04 11:22:51 +0200

    add favicon and set title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Stylish Portfolio - Start Bootstrap Template</title>
+    <title>Каталог товаров</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
